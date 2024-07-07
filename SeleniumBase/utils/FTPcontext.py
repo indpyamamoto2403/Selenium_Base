5fbc03105fee85d8882d2e73e995b4ce53390e8a
Yuki <163077361+indpyamamoto2403@users.noreply.github.com>
2024-07-08 08:17:31 +0900

    Update FTPcontext.py

--- SeleniumBase/utils/FTPcontext.py
+++ SeleniumBase/utils/FTPcontext.py
@@ -1,4 +1,6 @@
 from ftplib import FTP
+from logging import Logger
+from typing import Optional, List
 
 class FTPProcessor:
 
@@ -13,19 +15,19 @@ class FTPProcessor:
     ftp_processor.disconnect()
     '''
 
-    def __init__(self, ftp_host, ftp_port=21, ftp_user=None, ftp_password=None ,logger=None):
+    def __init__(self, ftp_host: str, ftp_port: int = 21, ftp_user: Optional[str] = None, ftp_password: Optional[str] = None, logger: Optional[Logger] = None) -> None:
         self.ftp_host = ftp_host
         self.ftp_port = ftp_port
         self.ftp_user = ftp_user
         self.ftp_password = ftp_password
-        self.ftp = None  # FTPオブジェクトの初期化
+        self.ftp: Optional[FTP] = None  # FTPオブジェクトの初期化
         
-        #ロガー設定
+        # ロガー設定
         self.logger = logger
 
         self.connect()
 
-    def connect(self):
+    def connect(self) -> None:
         try:
             self.ftp = FTP()
             self.ftp.connect(self.ftp_host, self.ftp_port, timeout=10)
@@ -34,30 +36,46 @@ class FTPProcessor:
         except Exception as e:
             print(f"Error connecting to FTP server: {e}")
             
-    def set_logger(self, logger):
+    def set_logger(self, logger: Logger) -> None:
         self.logger = logger
     
-    def get_list(self):
+    def get_list(self) -> List[str]:
         '''
         リストを表示させる方法。戻り値はlist[str]
         '''
-        
-        return self.ftp.retrlines('LIST')
+        files: List[str] = []
+        self.ftp.retrlines('LIST', files.append)
+        return files
 
-    def show_list(self):
-        if self.logger != None:
-            self.logger.debug(self.ftp.retrlines('LIST'))
+    def show_list(self) -> None:
+        if self.logger:
+            files = self.get_list()
+            for file in files:
+                self.logger.debug(file)
 
-    def download(self):
+    def download(self) -> None:
         pass
     
-    def upload(self):
-        pass
+    def upload(self, file_path: str, remote_path: str) -> None:
+        '''
+        ファイルをアップロードするメソッド
+        file_path: ローカルのファイルパス
+        remote_path: アップロード先のリモートのファイルパス
+        '''
+        try:
+            with open(file_path, 'rb') as file:
+                self.ftp.storbinary(f'STOR {remote_path}', file)
+                print(f"Uploaded {file_path} to {remote_path}")
+                if self.logger:
+                    self.logger.info(f"Uploaded {file_path} to {remote_path}")
+        except Exception as e:
+            print(f"Error uploading file: {e}")
+            if self.logger:
+                self.logger.error(f"Error uploading file: {e}")
     
-    def disconnect(self):
+    def disconnect(self) -> None:
         if self.ftp:
             self.ftp.quit()
             print("Disconnected from FTP server")
 
     # その他のFTP処理メソッドを追加することができます
-
